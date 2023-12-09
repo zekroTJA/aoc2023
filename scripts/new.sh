6cdf9737c8b6979a6e22e75beef69ab78089c841
zekroTJA <riho@live.de>
2023-12-09 11:10:13 +0100

    fix new script

--- scripts/new.sh
+++ scripts/new.sh
@@ -6,8 +6,8 @@ source "$PWD/.env" || { echo "No .env file found in the current directory!"; exi
 
 current_day=$(find . -maxdepth 1 -type d -name "day-*" | tail -1 | cut -c 3-)
 current_day=${current_day/*-}
-next_day="$(( current_day + 1 ))"
-next_day_padded=$(printf "%02d" $next_day)
+next_day="$(expr $current_day + 1)"
+next_day_padded=$(printf "%02d" "$next_day")
 
 if [ "$current_day" == "" ]; then
     insert_after="lib"
